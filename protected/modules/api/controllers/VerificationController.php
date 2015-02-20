c24a342a8ce41463358759a674182d92ad00b086
Anton <antbudko@gmail.com>
2015-02-20 15:52:32 +0400

    Update user status

--- protected/modules/api/controllers/VerificationController.php
+++ protected/modules/api/controllers/VerificationController.php
@@ -29,37 +29,50 @@ class VerificationController extends MainController {
     }
 
     public function actionSendDocs() {
-        
-        if (isset($_FILES) && count($_FILES) > 0) {
-            $docs = array();
-            foreach ($_FILES as $key => $value) {
-                $file = new File();
-                $file->fileName = $value['name'];
-                $file->fileSize = $value['size'];
-                $file->fileItem = new CUploadedFile($value['name'], $value['tmp_name'], $value['type'], $value['size'], $value['error']);
-                $file->uid = md5($this->user->id.$file->fileName.$file->fileSize.TIME);
-                $file->createdAt = TIME;
-                $file->createdBy = $this->user->id;
-                $file->entityType = 'doc';
+        try {
+            if (isset($_FILES) && count($_FILES) > 0) {
+                $docs = array();
+                foreach ($_FILES as $key => $value) {
+                    $file = new File();
+                    $file->fileName = $value['name'];
+                    $file->fileSize = $value['size'];
+                    $file->fileItem = new CUploadedFile($value['name'], $value['tmp_name'], $value['type'], $value['size'], $value['error']);
+                    $file->uid = md5($this->user->id.$file->fileName.$file->fileSize.TIME);
+                    $file->createdAt = TIME;
+                    $file->createdBy = $this->user->id;
+                    $file->entityType = 'doc';
 
-                if ($file->save()) {
-                    $path = Yii::getPathOfAlias('webroot') . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR . $file->uid;
-                    $file->fileItem->saveAs($path);
-                    $docs[] = $file;
-                } else {
-                    Response::ResponseError($file->getErrors());
+                    if ($file->save()) {
+                        $path = Yii::getPathOfAlias('webroot') . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR . $file->uid;
+                        $file->fileItem->saveAs($path);
+                        $docs[] = $file;
+                    } else {
+                        Response::ResponseError($file->getErrors());
+                    }
                 }
+
+                $user = User::model()->findByPk($this->user->id);
+                if(!$user) {
+                    throw new Exception();
+                }
+
+                $user->verifiedStatus = 'waitingForModeration';
+                if(!$user->save(true, array('verifiedStatus'))) {
+                    throw new Exception('Can\'t update user status');
+                }
+
+                Ticket::create(array(
+                    'title' => 'Verification',
+                    'department' => 'verification',
+                        ), 'New verification documents', $this->user->id, $docs);
+
+                Loger::logUser(Yii::app()->user->id, 'New verification documents');
+                Response::ResponseSuccess();
+            } else {
+                $this->actionPreflight();
             }
-
-            $ticket = Ticket::create(array(
-                'title' => 'Verification',
-                'department' => 'verification',
-                    ), 'New verification documents', $this->user->id, $docs);
-
-            Loger::logUser(Yii::app()->user->id, 'New verification documents');
-            Response::ResponseSuccess();
-        } else {
-            $this->actionPreflight();
+        } catch(Exception $e) {
+            Response::ResponseError($e->getMessage());
         }
     }
 
