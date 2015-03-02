2ec4c8b4f332cf8e588efeeef13b6b48c2718d21
Anton <antbudko@gmail.com>
2015-03-02 16:11:20 +0400

    PHPMailer

--- protected/components/MailSender.php
+++ protected/components/MailSender.php
@@ -3,6 +3,8 @@
 class MailSender extends CComponent {
     
     public static function sendEmail($type, $to, $data=array()) {
+        require_once dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.'/extensions/PHPMailer/PHPMailerAutoload.php';
+        
         $viewPath = 'views'.DIRECTORY_SEPARATOR.'mail'.DIRECTORY_SEPARATOR.$type.'.php';
         $fullPath = dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.$viewPath;
         
@@ -12,9 +14,33 @@ class MailSender extends CComponent {
         
         $fullMessagePath = Yii::app()->controller->renderFile($fullPath, $data, true);
 
-        $headers = 'Content-type: text/html; charset=utf-8' . "\r\n";
+        //$headers = 'Content-type: text/html; charset=utf-8' . "\r\n";
         
-        $status = mail($to, isset($data['subject'])?$data['subject']:'Rocket BTC', $fullMessagePath, $headers);
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->CharSet    = 'UTF-8';
+        $mail->Host       = 'mail.spacebtc.com'; 
+        $mail->SMTPAuth   = true;
+        $mail->Port       = 25;
+        $mail->Username   = 'noreply@spacebtc.com';
+        $mail->Password   = '43sahTMT4b';    
+        
+        $mail->SMTPDebug  = 1;
+        
+        $mail->From = 'noreply@spacebtc.com';
+        $mail->isHTML(true);
+        $mail->addAddress($to);
+        $mail->Subject = isset($data['subject'])?$data['subject']:'Rocket BTC';
+        $mail->Body = $fullMessagePath;
+        
+        $status = $mail->send();
+        if(!$status) {
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+            die();
+        }
+        
+        //$status = mail($to, isset($data['subject'])?$data['subject']:'Rocket BTC', $fullMessagePath, $headers);
 
         return $status;
     }
