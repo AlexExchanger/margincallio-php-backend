122d2054840860627a8db6759518286fb8d3e33b
Anton <antbudko@gmail.com>
2015-03-05 19:03:49 +0400

    Headers header

--- protected/modules/api/controllers/GatewayController.php
+++ protected/modules/api/controllers/GatewayController.php
@@ -24,6 +24,7 @@ class GatewayController extends MainController {
         $content_type = 'application/json';
         $status = 200;
 
+        header("Access-Control-Request-Headers:origin, content-type, accept, methods");
         header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
         header('Access-Control-Allow-Credentials: true');
         if (isset($_SERVER['HTTP_ORIGIN'])) {
