c980d02c8959180dc3cdb10c85ff8b5df1d3255f
Anton <antbudko@gmail.com>
2015-03-13 18:24:36 +0400

    New mail style

--- protected/views/mail/lostPassword.php
+++ protected/views/mail/lostPassword.php
@@ -1,3 +1,98 @@
-Rocket BTC
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Message Subject or Title</title>
+    <style type="text/css">
+        #outlook a {padding:0;}
+        body{width:100% !important; -webkit-text-size-adjust:100%; -ms-text-size-adjust:100%; margin:0; padding:0;}
+        .ExternalClass {width:100%;}
+        .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height: 100%;}
+        #backgroundTable {margin:0; padding:0; width:100% !important; line-height: 100% !important;}
+        img {outline:none; text-decoration:none; -ms-interpolation-mode: bicubic;}
+        a img {border:none;}
+        .image_fix {display:block;}
+        p {margin: 1em 0;}
+        h1, h2, h3, h4, h5, h6 {color: black !important;}
+        h1 a, h2 a, h3 a, h4 a, h5 a, h6 a {color: blue !important;}
+        h1 a:active, h2 a:active,  h3 a:active, h4 a:active, h5 a:active, h6 a:active {
+            color: red !important;
+        }
 
-Hello! You just used the "Lost password" option. Please follow <a href="<?= Yii::app()->params->currentUrl.'auth/user/changePassword/'.$confirmCode?>">This link</a> to change your current password.
+        h1 a:visited, h2 a:visited,  h3 a:visited, h4 a:visited, h5 a:visited, h6 a:visited {
+            color: purple !important;
+        }
+        table td {border-collapse: collapse;}
+        table { border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt; }
+        a {color: orange;}
+
+        @media only screen and (max-device-width: 480px) {
+            a[href^="tel"], a[href^="sms"] {
+                text-decoration: none;
+                color: blue; /* or whatever your want */
+                pointer-events: none;
+                cursor: default;
+            }
+
+            .mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
+                text-decoration: default;
+                color: orange !important;
+                pointer-events: auto;
+                cursor: default;
+            }
+
+        }
+
+        @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
+            a[href^="tel"], a[href^="sms"] {
+                text-decoration: none;
+                color: blue; /* or whatever your want */
+                pointer-events: none;
+                cursor: default;
+            }
+
+            .mobile_link a[href^="tel"], .mobile_link a[href^="sms"] {
+                text-decoration: default;
+                color: orange !important;
+                pointer-events: auto;
+                cursor: default;
+            }
+        }
+    </style>
+</head>
+<body>
+<table cellpadding="0" cellspacing="0" border="0" id="backgroundTable" width="100%">
+    <tbody><tr>
+        <td valign="top" style="background-color: #04708b">
+            <table cellpadding="0" cellspacing="0" border="0" align="center" width="100%" style="margin-bottom: 45px;">
+                <tbody><tr>
+                    <td valign="top" style="background-color: #002a48" width="5%"></td>
+                    <td valign="top" style="background-color: #002a48; padding-top:10px; padding-bottom: 10px;" width="90%"><a href="http://spacebtc.com/"><img class="image_fix" src="./Your Message Subject or Title_files/logo.png" alt="SpaceBTC" title="SpaceBTC" width="" height="x"></a></td>
+                    <td valign="top" style="background-color: #002a48" width="5%"></td>
+                </tr>
+            </tbody></table>
+            <table cellpadding="0" cellspacing="0" border="0" width="100%">
+                <tbody><tr>
+                    <td width="5%"></td>
+                    <td width="90%" style="background-color: #ffffff; padding: 35px;">
+
+                        <h1 style="color: #000000;">SpaceBTC</h1>
+
+                        <p style="color: #000000;">You just used the "Lost password" option. Please click the link below to change your current password:</p>
+                        <p style="color: #000000;">Link:<br><a href="<?= Yii::app()->params->currentUrl.'auth/user/changePassword/'.$confirmCode?>" style="color: #0f88a5;"><?= Yii::app()->params->currentUrl.'auth/user/changePassword/'.$confirmCode?></a></p>
+                        <p style="color: #000000;">Bitcoin can also be seen as the most prominent triple entry bookkeeping system in existence.</p>
+
+                    </td>
+                    <td width="5%"></td>
+                </tr>
+            </tbody></table>
+            <table cellpadding="0" cellspacing="0" border="0" width="100%">
+                <tbody><tr>
+                    <td style="height: 50px;"></td>
+                </tr>
+            </tbody></table>
+        </td>
+    </tr>
+</tbody></table>
+
+</body></html>
